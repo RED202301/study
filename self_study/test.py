ad237d2e4637564454feac707b4fcf4abbe9c169
홍진환 <ghdtka1218@gmail.com>
2023-01-17 15:10:47 +0900

    study

--- self_study/test.py
+++ self_study/test.py
@@ -1,15 +1,34 @@
 # i = 1
-result = 0
-for i in range(1,1000):
-    # a = 2 * i
-    # b = 7 * i
-    # if a < 1000 and a != b:
-    #     print(a)
-    #     result += a
-    # if b < 1000:
-    #     print(b)
-    #     result += b
-    if i%2 == 0 or i%7 == 0:
-        result += i
-print(result)
+# result = 0
+# for i in range(1,1000):
+#     # a = 2 * i
+#     # b = 7 * i
+#     # if a < 1000 and a != b:
+#     #     print(a)
+#     #     result += a
+#     # if b < 1000:
+#     #     print(b)
+#     #     result += b
+#     if i%2 == 0 or i%7 == 0:
+#         result += i
+# print(result)
 
+# sum = 0
+# for i in range(1, 1000):
+#     if i % 2 == 0:
+#         sum += i
+#     if i % 7 == 0:
+#         sum += i
+# print(sum)
+
+
+
+# [print(num) for num in [1,2,3]]
+# [print(num**2) for num in [1,2,3] if num % 2]
+
+m = 5
+n = 4
+
+for i in range(m):
+    # for j in range(n):
+    print('*' * n)
