04d1e22b90ae01e385b780eee992003dc447add0
홍진환 <ghdtka1218@gmail.com>
2023-03-16 14:24:01 +0900

    2023_std

--- algorithm/2884/sol.py
+++ algorithm/2884/sol.py
@@ -1,4 +1,29 @@
 import sys
 sys.stdin = open("input.txt")
+T = int(input())
+for _ in range(T):
+    n = int(input())
+    num = []
+    k = []
+    for i in range(2, n):
+        cnt = 0
+        for j in range(2, i):
+            if i % j == 0:
+                cnt += 1
+        if cnt == 0:
+            num.append(i)
 
-T =
+
+    result = n
+    for l in range(len(num)):
+        for p in range(len(num)):
+            if num[p]+num[l] == n:
+                if num[p] == num[l] == 0:
+                    k = num[p], num[l]
+                else:
+                    if result > abs(num[p]-num[l]):
+                        result = abs(num[p]-num[l])
+                        k = num[l], num[p]
+
+
+    print(*k)
